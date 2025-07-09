088b0d4e40c9868ad185ad23e117d73e3aadfcbc
larissa <rissapins@gmail.com>
2025-07-09 19:07:20 -0300

    feat: cria o CRUD da categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriaModule } from './categoria/categoria.module';
+import { Categoria } from './categoria/entities/categoria.entity';
 
 @Module({
   imports: [
@@ -12,9 +14,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [],
+      entities: [Categoria],
       synchronize: true,
     }),
+    CategoriaModule
   ],
   controllers: [],
   providers: [],
